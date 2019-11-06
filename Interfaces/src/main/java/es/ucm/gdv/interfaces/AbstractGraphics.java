b83cbad1ebd6b2d6b5b380c49f3eaa40b4fb0aea
adralcan <adrian97ad@gmail.com>
2019-11-06 21:32:44 +0100

    Hecho el escalado y el posicionamiento correcto. Revertir algunos cambios del commit anterior

--- Interfaces/src/main/java/es/ucm/gdv/interfaces/AbstractGraphics.java
+++ Interfaces/src/main/java/es/ucm/gdv/interfaces/AbstractGraphics.java
@@ -1,36 +1,39 @@
 package es.ucm.gdv.interfaces;
 
 public abstract class AbstractGraphics implements Graphics {
+    private final int baseWidthResolution = 9;
+    private final int baseHeighResolution = 16;
+    private final int baseSizeWidth = 1080;
+    private final int baseSizeHeight = 1920;
 
+    private int actualResolutionWidth;
     //x e y estan en coordenadas ""logicas"" de canvas/juego
     public Rect Escalamelo(Image image) {
         //https://stackoverflow.com/questions/10245220/java-image-resize-maintain-aspect-ratio
         int original_width = image.getWidth();
         int original_height = image.getHeight();
-        int bound_width = this.getWidth();
-        int bound_height = this.getHeight();
         int new_width = original_width;
-        int new_height = original_height;
         int x = 0;
         int y = 0;
 
-        // first check if we need to scale width
-        if (original_width > bound_width) {
-            //scale width to fit
-            new_width = bound_width;
-            //scale height to maintain aspect ratio
-            new_height = (new_width * original_height) / original_width;
-            y += bound_height / 2 - new_height / 2;
+        if (this.getWidth() > this.getHeight()) {
+            new_width = calculateNewAspectRatio(this.getHeight());
         }
+        else {
+            new_width = calculateNewAspectRatio(this.getWidth());
+        }
+        actualResolutionWidth = new_width;
+        x += (this.getWidth() / 2) - (new_width / 2);
+        new_width += x;
+        return new Rect(x, y, new_width, this.getHeight());
+    }
 
-        // then check if we need to scale even with the new height
-        if (new_height > bound_height) {
-            //scale height to fit instead
-            new_height = bound_height;
-            //scale width to maintain aspect ratio
-            new_width = (new_height * original_width) / original_height;
-            x += bound_width / 2 - new_width / 2;
-        }
-        return new Rect(x, y, new_width, new_height);
+    public int calculateNewAspectRatio(int param) {
+        return baseWidthResolution * param / baseHeighResolution;
+    }
+    public Pair calculateNewCoordinates(Pair logicCoords) {
+        int newX = logicCoords._first * actualResolutionWidth / baseSizeWidth;
+        int newY = logicCoords._second * this.getHeight() / baseSizeHeight;
+        return new Pair(newX, newY);
     }
 }
